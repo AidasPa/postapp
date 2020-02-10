e70f0df9714843d06bda7bb39e5840e70dde1d89
Aidas Patas <40808754+AidasPa@users.noreply.github.com>
2020-02-10 20:28:15 +0200

    Update minui.router.js

--- src/modules/minui.router.js
+++ src/modules/minui.router.js
@@ -1,77 +1,75 @@
-import MinuiFramework from "../modules/minui.core";
+import MinuiFramework from '../modules/minui.core'
 
-if (typeof Array.prototype.reIndexOf === "undefined") {
-  Array.prototype.reIndexOf = function(rx) {
+if (typeof Array.prototype.reIndexOf === 'undefined') {
+  Array.prototype.reIndexOf = function (rx) {
     for (var i in this) {
       if (this[i].toString().match(rx)) {
-        return i;
+        return i
       }
     }
-    return -1;
-  };
+    return -1
+  }
 }
 
 class MinuiRouter extends MinuiFramework {
-  constructor(routes, errors) {
-    super();
-    this.appRoutes = routes;
-    this.appErrors = errors;
-    this.routeData = {};
-    this.initialize_router();
+  constructor (routes, errors) {
+    super()
+    this.appRoutes = routes
+    this.appErrors = errors
+    this.routeData = {}
+    this.initialize_router()
+
     window.onpopstate = e => {
-      
-      this.route_and_render(e.state.path);
-    };
+      this.route_and_render(e.state.path, true)
+    }
   }
 
-  initialize_router() {
-    let errorsIndex = 0;
+  initialize_router () {
+    let errorsIndex = 0
     for (var routeName in this.appRoutes) {
       var pathMatch = new RegExp(
-        "^" + this.appRoutes[routeName].path.replace(/:(.*)/, "(.*)") + "$"
-      );
+        '^' + this.appRoutes[routeName].path.replace(/:(.*)/, '(.*)') + '$'
+      )
       if (pathMatch.test(window.location.pathname)) {
         if (/:(.*?)/.test(this.appRoutes[routeName].path)) {
           const nthVariable = this.appRoutes[routeName].path
-            .split("/")
-            .reIndexOf(/:(.*?)/);
-          const variableName = this.appRoutes[routeName].path.match(
-            /\:(.*)/
-          )[1];
+            .split('/')
+            .reIndexOf(/:(.*?)/)
+          const variableName = this.appRoutes[routeName].path.match(/\:(.*)/)[1]
           this.routeData = {
             params: {
-              [variableName]: window.location.pathname.split("/")[nthVariable]
+              [variableName]: window.location.pathname.split('/')[nthVariable]
             }
-          };
+          }
           this.render(
             this.appRoutes[routeName].renderView(this.routeData).html(),
             { route: this.routeData },
             this.appRoutes[routeName].renderView(this.routeData).func()
-          );
-          this.appRoutes[routeName].onload(this.routeData);
+          )
+          this.appRoutes[routeName].onload(this.routeData)
         } else {
           this.render(
             this.appRoutes[routeName].renderView.html(),
             {},
             this.appRoutes[routeName].renderView.func()
-          );
-          this.appRoutes[routeName].onload();
+          )
+          this.appRoutes[routeName].onload()
         }
-        break;
+        break
       } else {
-        errorsIndex++;
+        errorsIndex++
         if (errorsIndex == Object.keys(this.appRoutes).length) {
           this.render(this.appErrors[404].view)
-          break;
+          break
         }
       }
     }
   }
-  route_and_render(path) {
-    this.renderTimes.push(path);
+  route_and_render(path, is) {
+    this.renderTimes.push(path)
 
-    history.pushState({ path: path }, path, path);
-    this.initialize_router();
+    if (!is) history.pushState({ path: path }, path, path)
+    this.initialize_router()
   }
 }
-export default MinuiRouter;
+export default MinuiRouter
